5afd03de47bea0683e721f507a647b3fc48a6a49
Ivan <ivandefla@gmail.com>
2025-05-09 22:38:51 -0300

    Create Program.cs

---
+++ Questão1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Prog
+{
+    static void Main()
+    {
+
+        int I = 13;
+        int SOMA = 0;
+        int K = 0;
+
+
+        while (K < I)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+
+        Console.WriteLine(SOMA);
+    }
+}
