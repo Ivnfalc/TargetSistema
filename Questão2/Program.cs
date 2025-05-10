db019a5a25919e723635b27fb6ce3d3abee2f55b
Ivan <ivandefla@gmail.com>
2025-05-09 22:31:25 -0300

    Correção nome

---
+++ Questão2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Prog
+{
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int num = int.Parse(Console.ReadLine());
+
+        bool pertence = false;
+        int a = 0;
+        int b = 1;
+
+        while (a <= num)
+        {
+            if (a == num)
+            {
+                pertence = true;
+                break;
+            }
+            int prx = a + b;
+            a = b;
+            b = prx;
+        }
+
+        if (pertence)
+        {
+            Console.WriteLine($"número {num} pertence à sequência");
+        }
+        else
+        {
+            Console.WriteLine($"número {num} não pertence à sequência");
+        }
+    }
+}
---
+++ Questão4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Prog
+{
+    static void Main()
+    {
+        var fat = new Dictionary<string, double>();
+        fat.Add("SP", 67836.43);
+        fat.Add("RJ", 36678.66); 
+        fat.Add("MG", 29229.88);
+        fat.Add("ES", 27165.48);
+        fat.Add("Outros", 19849.53);
+
+        double soma = 0;
+        foreach (var x in fat)
+        {
+            soma = soma + x.Value;
+        }
+
+        Console.WriteLine("Percentual de cada um:");
+        foreach (KeyValuePair<string, double> i in fat)
+        {
+            double p = (i.Value / soma) * 100;
+            Console.WriteLine(i.Key + " -> " + p.ToString("F2") + "%");
+        }
+
+        // fim :)
+    }
+}
