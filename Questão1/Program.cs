3c37171958fadb519c154422926d6336d233dc14
Ivan <ivandefla@gmail.com>
2025-05-09 22:23:38 -0300

    Avaliação

---
+++ Questão1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Prog
+{
+    static void Main()
+    {
+        int lim = 13;
+        int tot = 0;
+        for (int cont = 1; cont <= lim; cont++)
+        {
+            tot += cont; 
+        }
+        Console.WriteLine(tot);
+    }
+}
